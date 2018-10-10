fc1f6373f1608919912ae71da2310492fec764a4
pablo <pcoronel@untref.edu.ar>
2018-10-10 18:19:07 -0300

    agregdao efecto de girar las cartas

--- src/Carta.js
+++ src/Carta.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './Carta.css';
+import FlipCard from 'react-flipcard-2';
 
 export default class Carta extends Component {
 	render() {
 		return (
 			<div className="carta">
-  			<i className={'fa ' + this.props.carta + ' fa-5x'} />
+				<FlipCard>
+					<div className="portada" />
+					<div className="contenido">
+						<i className={'fa ' + this.props.carta + ' fa-5x'} />
+					</div>
+				</FlipCard>
 			</div>
 		);
 	}
