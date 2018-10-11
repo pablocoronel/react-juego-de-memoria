3c47fb4851952ee28f2cd7c62d659d3f1813f022
pablo <pcoronel@untref.edu.ar>
2018-10-11 15:00:46 -0300

    hecho video 11, clickea cartas

--- src/Carta.js
+++ src/Carta.js
@@ -5,11 +5,18 @@ import FlipCard from 'react-flipcard-2';
 export default class Carta extends Component {
 	render() {
 		return (
-			<div className="carta">
-				<FlipCard>
+			<div className="carta" onClick={this.props.seleccionarCarta}>
+				<FlipCard
+					// en true se queda volteada
+					flipped={
+						this.props.estaSiendoComparada ||
+						this.props.fueAdivinada
+					}
+					disabled={true}
+				>
 					<div className="portada" />
 					<div className="contenido">
-						<i className={'fa ' + this.props.carta + ' fa-5x'} />
+						<i className={'fa ' + this.props.icono + ' fa-5x'} />
 					</div>
 				</FlipCard>
 			</div>
--- src/Tablero.js
+++ src/Tablero.js
@@ -6,9 +6,29 @@ export default class Tablero extends Component {
 	render() {
 		return (
 			<div className="tablero">
-				{this.props.baraja.map((carta, index) => (
-					<Carta carta={carta.icono} key={index}/>
-				))}
+				{/* para cada carta.. */}
+				{this.props.baraja.map((carta, index) => {
+					// Boolean de cada carta para saber si la carta esta en el array de comparaciones
+					const estaSiendoComparada =
+						this.props.parejaSeleccionada.indexOf(carta) > -1;
+
+					return (
+						<Carta
+							// lo pide react
+							key={index}
+							// icono para mostrar
+							icono={carta.icono}
+							// boolean para saber si esta siendo comparada
+							estaSiendoComparada={estaSiendoComparada}
+							// funcion que usa la carta
+							seleccionarCarta={() =>
+								this.props.seleccionarCarta(carta)
+							}
+							// boolean si fue adivinada
+							fueAdivinada={carta.fueAdivinada}
+						/>
+					);
+				})}
 			</div>
 		);
 	}
