b5807ee4484ad96dfa57628d611a668e8b42941d
pablo <pcoronel@untref.edu.ar>
2018-10-11 13:27:02 -0300

    hecho video 10, conecta el metodo de seleccion con el componente Carta

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Header from './Header';
 import Tablero from './Tablero';
 import construirBaraja from './utils/construirBaraja';
 
-// Estado
 const getEstadoInicial = () => {
 	const baraja = construirBaraja();
 
 	return {
-		baraja
+		baraja,
+		parejaSeleccionada: [],
+		estaComparando: false
 	};
 };
 
@@ -20,13 +21,40 @@ class App extends Component {
 		this.state = getEstadoInicial();
 	}
 
+	// Estado inicial
 	render() {
 		return (
 			<div className="App">
 				<Header />
-				<Tablero baraja={this.state.baraja} />
+
+				{/* Tablero recibe: la baraja con todas las cartas disponibles, el estado actual del contenedor de cartas a comparar, la funcion para seleccionar una carta (la va a usar el coponente Carta) */}
+				<Tablero
+					baraja={this.state.baraja}
+					parejaSeleccionada={this.state.parejaSeleccionada}
+					seleccionarCarta={(carta) => this.seleccionarCarta(carta)}
+				/>
 			</div>
 		);
+	}
+
+	//Selecciona la carta clickeada
+	seleccionarCarta(carta) {
+		// NO selecciona la carta si: el usuario esta comparando, la carta clickeada YA EXISTE en las seleccionadas, la carta fue adivinada
+		if (
+			this.state.estaComparando ||
+			this.state.parejaSeleccionada.indexOf(carta) > -1 ||
+			carta.fueAdivinada
+		) {
+			return;
+		}
+
+		// Agregar la carta clickeada al array que guarda las cartas a comparar:
+		const pareja = [...this.state.parejaSeleccionada, carta];
+
+		// actualiza el array de comparacion en  el state:
+		this.setState({
+			parejaSeleccionada: pareja
+		});
 	}
 }
 
